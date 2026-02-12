3246f9534c5aedc4d7d7a0edd488fbbfef8c95ca
IDIC <alumnos@alumnosuio.ute.edu.ec>
2026-02-11 21:15:27 -0500

    11/2/1015

--- cine_api/catalog/models.py
+++ cine_api/catalog/models.py
@@ -1,14 +1,14 @@
 
 from django.db import models
 
-class Marca(models.Model):
+class numeroBoleto(models.Model):
     nombre = models.CharField(max_length=120, unique=True)
 
     def __str__(self):
         return self.nombre
 
 class Cine(models.Model):
-    marca = models.ForeignKey(Marca, on_delete=models.PROTECT, related_name="vehiculos")
+    numeroBoleto = models.ForeignKey(numeroBoleto, on_delete=models.PROTECT, related_name="vehiculos")
     modelo = models.CharField(max_length=120)
     anio = models.IntegerField()
     placa = models.CharField(max_length=20, unique=True)
@@ -16,4 +16,4 @@ class Cine(models.Model):
     creado_en = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.marca.nombre} {self.modelo} ({self.placa})"
+        return f"{self.numeroBoleto.nombre} {self.modelo} ({self.placa})"
